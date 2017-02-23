557a7faa1e697a4d3c6d25a55a0935ccbf0794bc
Alex KPENOU <akpenou@e1r10p18.42.fr>
2017-02-23 21:55:06 +0100

    [UPD] parse.py

--- parse.py
+++ parse.py
@@ -27,5 +27,40 @@ def parse(filename):
 			reqs.append(parse_req(f))
 	return meta, videos, endpoints, reqs
 
+def ep_by_video(videos, reqs):
+	res = list()
+	for index, size in enumerate(videos):
+		tmp = dict()
+		tmp['_id'] = index
+		tmp['size'] = size
+		tmp['endpoints'] = list(filter(lambda x: x['video_id'] == index, reqs))
+		tmp['total_req'] = sum([ x['nb_req'] for x in tmp['endpoints'] ])
+		res.append(tmp)
+	return res
+
+def caches(endpoints, meta):
+	nb_caches = meta['caches']
+	caches = [ dict() for i in range(nb_caches + 1) ]
+	return caches
+	for index, endpoint in enumerate(endpoints):
+		for _id, latence in endpoint['caches']:
+			caches[_id].append({'index': index, 'latence': latence})
+		caches[nb_caches].append((index, endpoint['to_serve']))
+	return caches
+
+
 meta, videos, endpoints, reqs = parse('me_at_the_zoo.in')
-df_reqs = pd.DataFrame(reqs)
+tmp = ep_by_video(videos, reqs)
+tmp.sort(key = lambda x: x['total_req'], reverse = True)
+lst_caches = caches(endpoints, meta)
+for video in tmp:
+	if meta['size'] > video['size']:
+		meta['size'] -= video['size']
+		for index in range(len(lst_caches)):
+			if not 'videos' in lst_caches[index].keys():
+				lst_caches[index]['videos'] = list()
+			lst_caches[index]['videos'].append(video['_id'])
+
+print(meta['caches'])
+for index in range(meta['caches']):
+	print(index, ' '.join(map(str, lst_caches[index]['videos'])))
